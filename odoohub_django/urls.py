8c3924d39a15d284fd221f2e18cf94890880b637
huakkai <huaqiangyan@163.com>
2021-03-02 11:32:47 +0800

    [IMP]viewflow

--- odoohub_django/urls.py
+++ odoohub_django/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views import generic
+from material.frontend import urls as frontend_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', generic.RedirectView.as_view(url='/workflow/', permanent=False)),
+    url(r'', include(frontend_urls)),
 ]
